a7d6605bf0ceb08e806984eb80211dca82471289
Gabriel Ravelo <ravelogabriel2@gmail.com>
2022-08-04 17:05:18 -0400

    Se adelanto hasta la implementacion del bootstrap y vuejs

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/recetas', 'App\Http\Controllers\RecetaController');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
