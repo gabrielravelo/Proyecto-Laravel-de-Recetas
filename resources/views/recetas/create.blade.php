71a0bbdeb5bd34bc25a932185b2e4452b8d36c5e
Gabriel Ravelo <ravelogabriel2@gmail.com>
2022-08-07 12:43:42 -0400

    se termino el capitulo 7

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css" integrity="sha512-CWdvnJD7uGtuypLLe5rLU3eUAkbzBR3Bm1SFPEaRfvXXI2v2H5Y0057EMTzNuGGRIznt8+128QIDQ8RqmHbAdg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+@endsection
+
 @section('botones')
     <a href="{{ route('recetas.index') }}" class="btn btn-primary mr-2">Volver</a>
 
 @endsection
 
 @section('content')
-    <h2 class="text-center mb-5">Crear Nueva Receta</h2> 
+    <h2 class="text-center mb-5">Crear Nueva Receta</h2>
 
     <div class="row justify-content-center mt-5">
 
       <div class="col-md-8">
-        <form method="POST" action="{{ route('recetas.store') }}" novalidate>
+        <form method="POST" action="{{ route('recetas.store') }}" enctype="multipart/form-data" novalidate>
           @csrf
 
           <div class="form-group">
             <label for="titulo">Título Receta</label>
 
-            <input type="text" 
-                    name="titulo" 
-                    class="form-control 
-                    @error('titulo') is-invalid @enderror" 
-                    id="titulo" 
-                    placeholder="Título Receta" 
+            <input type="text"
+                    name="titulo"
+                    class="form-control
+                    @error('titulo') is-invalid @enderror"
+                    id="titulo"
+                    placeholder="Título Receta"
                     value={{ old('titulo') }}
             >
 
@@ -50,6 +54,41 @@
             @enderror
           </div>
 
+          <div class="form-group mt-3">
+            <label for="preparacion">Preparación</label>
+            <input id="preparacion" type="hidden" name="preparacion" value="{{ old('preparacion') }}">
+            <trix-editor class="form-control @error('preparacion') is-invalid @enderror" input="preparacion"></trix-editor>
+
+            @error('preparacion')
+              <span class="invalid-feedback d-block" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+          </div>
+
+          <div class="form-group mt-3">
+            <label for="ingredientes">Ingredientes</label>
+            <input id="ingredientes" type="hidden" name="ingredientes" value="{{ old('ingredientes') }}">
+            <trix-editor class="form-control @error('ingredientes') is-invalid @enderror" input="ingredientes"></trix-editor>
+
+            @error('ingredientes')
+              <span class="invalid-feedback d-block" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+          </div>
+
+          <div class="form-group mt-3">
+            <label for="imagen">Sube una Imágen</label>
+            <input id="imagen" type="file" class="form-control @error('imagen') is-invalid @enderror" name="imagen">
+
+            @error('imagen')
+              <span class="invalid-feedback d-block" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+          </div>
+
           <div class="form-group">
             <input type="submit" class="btn btn-primary" value="Agregar Receta">
           </div>
@@ -57,8 +96,11 @@
         </form>
       </div>
 
-    </div> 
+    </div>
 
 @endsection
 
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js" integrity="sha512-/1nVu72YEESEbcmhE/EvjH/RxTg62EKvYWLG3NdeZibTCuEtW5M4z3aypcvsoZw03FAopi94y04GhuqRU9p+CQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+@endsection
 
